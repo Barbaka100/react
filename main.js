222394c086171968a1d7ec20e380d14f0eba7fe1
barbaka100 <aleko_barbakadze@yahoo.com>
2023-05-08 15:20:14 +0400

    test

---
+++ main.js
@@ -0,0 +1,27 @@
+console.log('this is working')
+
+
+
+// const a = 9
+// const b = 5
+
+// if(a + b ===8){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+/* function calculate(num1, num2){
+    if(typeof num1 === 'string'){
+        return false
+    }
+
+    if(typeof num2 === 'string'){
+        return false
+    }
+    return num1 + num2
+}
+
+console.log(calculate(150, 60))
+console.log(calculate('alex', 60))
+console.log(calculate('150', 60)) */
---
+++ test.js
@@ -0,0 +1,31 @@
+
+/*const a = 3
+const b = 3
+
+if(a === b){
+    console.log('ტოლია')
+}else{
+    console.log('არ არის ტოლი')
+}
+
+return a === b*/
+
+// #####
+
+/*const a = 11
+const b = 12 
+
+if (a * b === 120){
+    console.log(120)
+}else{
+    console.log(false)
+}*/
+
+
+
+/*function temp(name){
+    return name
+}
+
+console.log(temp('ფარენგეიტი'))
+console.log(temp('ცელსიუსი'))*/
