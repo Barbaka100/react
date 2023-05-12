96726c2db4c7e8f8cda246eaa85c6b65a2fb5271
barbaka100 <aleko_barbakadze@yahoo.com>
2023-05-12 23:08:38 +0400

    react

--- test.js
+++ test.js
@@ -1,31 +1,64 @@
+      //#1
 
-/*const a = 3
-const b = 3
+// const user = [
+//     {name: 'temo',
+//     age: 25},
 
-if(a === b){
-    console.log('ტოლია')
-}else{
-    console.log('არ არის ტოლი')
-}
+//     {name: 'lasha',
+//     age: 21},
 
-return a === b*/
+//     {name: 'ana',
+//     age: 28}
+    
+// ]
 
-// #####
-
-/*const a = 11
-const b = 12 
-
-if (a * b === 120){
-    console.log(120)
-}else{
-    console.log(false)
-}*/
+// console.log(user [1])
 
 
+            //#2
 
-/*function temp(name){
-    return name
-}
 
-console.log(temp('ფარენგეიტი'))
-console.log(temp('ცელსიუსი'))*/
+// const user = {
+//     firstname: 'Alex',
+//     lastname: 'Barbakadze',
+//     age: 39,
+//     new: function(){
+//         console.log('new function')
+//     }
+   
+// }
+
+// console.log(user.firstname, user.lastname, user.age)
+// user.new()
+
+
+            //#3
+
+
+ 
+    
+    function Alex(){
+        return parseInt(Math.random() * 10) + 1
+    }
+    
+    let result1 = Alex()
+    
+    while(result1 !==3){
+        result1 = Alex()
+        console.log(result1 + ' ' + 'Alex')
+    }
+
+
+    function David(){
+        return parseInt(Math.random() * 10) + 1
+    }
+    
+    let result2 = David()
+    
+    while(result2 !==3){
+        result2 = David()
+        console.log(result2 + ' ' + 'David')
+    }
+
+    
+    
