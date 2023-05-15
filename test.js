ebfc11ccdbff34a678177a1a9c606227a4844c07
barbaka100 <aleko_barbakadze@yahoo.com>
2023-05-15 23:38:45 +0400

    react

--- test.js
+++ test.js
@@ -1,64 +1,20 @@
-      //#1
-
-// const user = [
-//     {name: 'temo',
-//     age: 25},
-
-//     {name: 'lasha',
-//     age: 21},
-
-//     {name: 'ana',
-//     age: 28}
-    
-// ]
-
-// console.log(user [1])
-
-
-            //#2
-
-
 // const user = {
-//     firstname: 'Alex',
-//     lastname: 'Barbakadze',
-//     age: 39,
-//     new: function(){
-//         console.log('new function')
-//     }
-   
+//     banks: 'BOG',
+//     adress: 'chavchavadze street',
+//     sity: 'rustavi',
 // }
 
-// console.log(user.firstname, user.lastname, user.age)
-// user.new()
 
 
-            //#3
+// const {sity, adress, banks, appartments} = user
+
+// console.log(sity, appartments)
 
 
- 
-    
-    function Alex(){
-        return parseInt(Math.random() * 10) + 1
-    }
-    
-    let result1 = Alex()
-    
-    while(result1 !==3){
-        result1 = Alex()
-        console.log(result1 + ' ' + 'Alex')
-    }
+const calculate = (a, b, c, d, e) => {
+    return a + b
+}
+
+console.log(calculate(5, 7))
 
 
-    function David(){
-        return parseInt(Math.random() * 10) + 1
-    }
-    
-    let result2 = David()
-    
-    while(result2 !==3){
-        result2 = David()
-        console.log(result2 + ' ' + 'David')
-    }
-
-    
-    
