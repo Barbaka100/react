db532f209ffa7ad77e18d2183589a78e66535ca4
barbaka100 <aleko_barbakadze@yahoo.com>
2023-05-16 00:14:19 +0400

    react

--- test.js
+++ test.js
@@ -5,8 +5,6 @@ const calculate = (a, b, c, d, e) => {
 console.log(calculate(5, 7))
 
 
-
-
 // const user = {
 //     banks: 'BOG',
 //     adress: 'chavchavadze street',
@@ -23,20 +21,28 @@ console.log(calculate(5, 7))
 
 
 
-// const user1 = {
-//     name: 'alex',
-//     lasName: 'barbakadze',
-//     age: 39,
-//     address: {
-//         country: 'georgia'
-//     }
-// }
+const user1 = {
+    name: 'alex',
+    lasName: 'barbakadze',
+    age: 39,
+    address: {
+        country: 'georgia'
+    }
+}
 
 
-// const user2 = {
-//     ...user1
-// }
+const user2 = {
+    ...user1,
+    address: {
+        ...user1.address
+    }
+     
+}
 
-// user2.name = 'david'
 
-// console.log(user2.name, user1.lasName)
+user2.name = 'david'
+user2.address = 'Italy'
+
+console.log(user2.name, user1.lasName)
+
+console.log(user2.address, user1.age)
