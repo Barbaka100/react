0d56e37d6bb56c666be3d147f613ec7e36c0f372
barbaka100 <aleko_barbakadze@yahoo.com>
2023-06-02 16:31:09 +0400

    react

---
+++ test1/lessons.js
@@ -0,0 +1,60 @@
+
+         // davaleva #1
+/* function equal(a ,b){
+    if (a !== b) return 'ar aris toli'; //false
+    return 'tolia'; // true
+}
+
+console.log(equal(2 , 2)); */
+
+
+         // davaleba #2
+
+
+/* function farenheitToCelcius (temp){
+    if (typeof temp !== 'number') return false;
+    return(temp - 32) / 1.8;
+}
+
+console.log(farenheitToCelcius(94)) */
+
+
+            //davaleba #3
+
+/* function calculate(a, b, op){
+    if(typeof a !== 'number' || typeof b !== 'number') return false;
+    switch (op) {
+        case '+' : return a + b;
+        case '-' : return a - b;
+        case '*' : return a * b;
+        case '/' : return a / b;
+        default: return false 
+    }
+}
+
+console.log(calculate(5, 8, '*')) */
+
+
+            // davaleba #4
+
+            // davaleba #5
+            
+            // davaleba #6
+
+
+/* const user = {
+    id: 1,
+    name: 'alex',
+    lastName: 'barbaka'
+}
+function copyUser(user) {
+    const newUser = {};
+    Object.entries(user).forEach(([key, value]) => newUser[key] = value)
+    return newUser
+}
+const usercopy = copyUser(user)
+user.id = 10
+console.log(user)
+console.log(usercopy)
+ */
+
